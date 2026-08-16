92b48dccdab8bf14997184d750aaa03054b8c2c6
Dasio96 <dawid01852@gmail.com>
2026-08-16 21:40:52 +0200

    feat(main): load ROM file from command line argument

--- main.c
+++ main.c
@@ -3,8 +3,18 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("%s\n", argv[0]);
+    return 1;
+  }
+
   Memory memory;
   CPU cpu;
+
+  if (!memory_load_rom(&memory, argv[1])) {
+    printf("%s\n", argv[1]);
+    return 1;
+  }
 
   cpu_init(&cpu, &memory);
 
